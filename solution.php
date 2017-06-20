9d31dc7586dd5fe83ca4f9f3c40908143464c0fa
marshclaud <marshallclaudius@outlook.com>
2017-06-20 04:22:17 -0700

    Update and rename solution.html to solution.php

--- solution.php
+++ solution.php
@@ -1,8 +1,127 @@
 <!DOCTYPE html>
     <head>
         <title>SSTechnoloy Solutions</title>
-        <link rel="stylesheet" type="text/css" href="marshclaud.github.io/styles.css" >
-        
+       <!-- <link rel="stylesheet" type="text/css" href="marshclaud.github.io/styles.css" >
+        -->
+        <style>
+            body {
+  background-color: #FFF;
+  
+  margin: 0;
+  
+}
+
+#padd{
+    padding-top: 50px;
+}
+
+#pagewrap{
+    
+padding-left: 200px;
+  padding-right: 200px;
+}
+
+.header {
+  background-image: url("http://www.freepngimg.com/download/networking/7-2-networking-png-hd.png");
+  background-position: center center;
+  background-size: cover;
+  height: 500px;
+  width: 100%;
+}
+
+#header-text{
+  margin: 0 auto;
+  position: relative;
+  text-align: center;
+  top: 25%;
+  width: 60%;
+
+}
+
+#topnav ul.nav {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    position: fixed;
+    top:0;
+    overflow: hidden;
+   border: 1px solid #e7e7e7;
+    background-color: #f3f3f3;
+}
+
+#topnav li {
+    float:left;
+     border-right:1px solid #bbb;
+}
+
+#topnav li:last-child {
+    border-right: none;
+}
+
+
+#topnav li a {
+    display: block;
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+   }
+#topnav li a:hover:not(.active) {
+    background-color: lightblue;
+}
+
+
+
+.active {
+    background-color:lightgreen;
+}
+
+#nav{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+h1 {
+  color: #f79209;
+   font-family: Arial, Helvetica, sans-serif;
+  font-size: 70px;
+  font-weight: 400;
+  line-height: 60px;
+  margin: 0;
+}
+
+
+h2 {
+  color:black;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 20px;
+  font-weight: 10;
+  line-height: 25px;
+  margin: 0;
+ 
+}
+
+#aboutus{
+     color: black;
+  font-family: 'Quicksand', sans-serif;
+  font-size: 50px;
+  font-weight: 400;
+  line-height: 60px;
+  margin: 0;
+}
+
+#us{
+    color: black;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 18px;
+  font-weight: 10;
+  line-height: 25px;
+  margin: 0;
+
+}
+
+        </style>
     </head>
 
     <body>
@@ -11,12 +130,12 @@
 
         <div id="topnav">
             <ul class="nav">
-                <li><a href="marshclaud.github.io/index.html">Home</a></li>
-                <li><a href="marshclaud.github.io/services.html">Services</a></li>
-                <li><a class="active" href="marshclaud.github.io/solution.html">Solutions</a></li>
-                <li><a href="marshclaud.github.io/turnkey.html">Turnkey</a></li>
-                <li><a href="marshclaud.github.io/about.html">About Us</a></li>
-                <li style="float:right"><a href="marshclaud.github.io/contact.html">Contact</a></li>
+                <li><a href="/index.html">Home</a></li>
+                <li><a href="/services.html">Services</a></li>
+                <li><a class="active" href="/solution.html">Solutions</a></li>
+                <li><a href="/turnkey.html">Turnkey</a></li>
+                <li><a href="/about.html">About Us</a></li>
+                <li style="float:right"><a href="/contact.html">Contact</a></li>
                 
 
             </ul>
