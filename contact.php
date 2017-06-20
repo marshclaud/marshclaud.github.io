c7c0c75681773b62d8840a72e9ea0fec2de3fe74
marshclaud <marshallclaudius@outlook.com>
2017-06-20 02:37:50 -0700

    Update and rename contact.html to contact.php

---
+++ contact.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+    <head>
+        <title>SSTechnologies Contact</title>
+        <link rel="stylesheet" type="text/css" href="marshclaud.github.io/styles.css" >
+        <style>
+            body {
+  background-color: #FFF;
+  
+  margin: 0;
+  
+}
+
+#padd{
+    padding-top: 50px;
+}
+
+#pagewrap{
+    
+padding-left: 200px;
+  padding-right: 200px;
+}
+
+.header {
+  background-image: url("http://www.freepngimg.com/download/networking/7-2-networking-png-hd.png");
+  background-position: center center;
+  background-size: cover;
+  height: 500px;
+  width: 100%;
+}
+
+#header-text{
+  margin: 0 auto;
+  position: relative;
+  text-align: center;
+  top: 25%;
+  width: 60%;
+
+}
+
+#topnav ul.nav {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    position: fixed;
+    top:0;
+    overflow: hidden;
+   border: 1px solid #e7e7e7;
+    background-color: #f3f3f3;
+}
+
+#topnav li {
+    float:left;
+     border-right:1px solid #bbb;
+}
+
+#topnav li:last-child {
+    border-right: none;
+}
+
+
+#topnav li a {
+    display: block;
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+   }
+#topnav li a:hover:not(.active) {
+    background-color: lightblue;
+}
+
+
+
+.active {
+    background-color:lightgreen;
+}
+
+#nav{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+h1 {
+  color: #f79209;
+   font-family: Arial, Helvetica, sans-serif;
+  font-size: 70px;
+  font-weight: 400;
+  line-height: 60px;
+  margin: 0;
+}
+
+
+h2 {
+  color:black;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 20px;
+  font-weight: 10;
+  line-height: 25px;
+  margin: 0;
+ 
+}
+
+#aboutus{
+     color: black;
+  font-family: 'Quicksand', sans-serif;
+  font-size: 50px;
+  font-weight: 400;
+  line-height: 60px;
+  margin: 0;
+}
+
+#us{
+    color: black;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 18px;
+  font-weight: 10;
+  line-height: 25px;
+  margin: 0;
+
+}
+
+        </style>
+    </head>
+
+    <body>
+        <div id="pagewrap"> 
+        <!-- naviation bar will come here -->
+
+       <div id="topnav">
+            <ul class="nav">
+                <li><a href="marshclaud.github.io/index.php">Home</a></li>
+                <li><a href="marshclaud.github.io/services.php">Services</a></li>
+                <li><a href="marshclaud.github.io/solution.php">Solutions</a></li>
+                <li><a href="marshclaud.github.io/turnkey.php">Turnkey</a></li>
+                <li><a href="marshclaud.github.io/about.php">About Us</a></li>
+               <li style="float:right"><a class="active" href="marshclaud.github.io/contact.php">Contact</a></li>
+                 
+
+            </ul>
+        </div>
+
+        <div id="padd">
+            </div>
+
+
+        <div>
+            <h1 align ='center'>S S Technologies</h1>
+                <p>
+                  <h3 align='center'>
+                       #1/B Acharya Building, Dhobi wadi, Thakurdwar JSS Road, Mumbai- 400002.</br>
+                       Tel:- +91 98923 25288| 77387 93521</br>
+                       Email: sstechnologies2016@outlook.com
+                    </h3>
+                </p>
+        </div>
+        </div>
+    </body>
+</html>
