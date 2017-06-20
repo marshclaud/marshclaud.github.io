bb53f540f4f45b55da889013079b9812703fcb13
marshclaud <marshallclaudius@outlook.com>
2017-06-20 04:26:29 -0700

    Update and rename turnkey.html to turnkey.php

---
+++ turnkey.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+    <head>
+        <title>SSTechnologies Turnkey Projects</title>
+        <!--<link rel="stylesheet" type="text/css" href="marshclaud.github.io/styles.css" >
+        -->
+        <style>
+            body {
+  background-color: #FFF;
+  
+  margin: 0;
+  
+}
+
+#padd{
+    padding-top: 50px;
+}
+
+#pagewrap{
+    
+padding-left: 200px;
+  padding-right: 200px;
+}
+
+.header {
+  background-image: url("http://www.freepngimg.com/download/networking/7-2-networking-png-hd.png");
+  background-position: center center;
+  background-size: cover;
+  height: 500px;
+  width: 100%;
+}
+
+#header-text{
+  margin: 0 auto;
+  position: relative;
+  text-align: center;
+  top: 25%;
+  width: 60%;
+
+}
+
+#topnav ul.nav {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    position: fixed;
+    top:0;
+    overflow: hidden;
+   border: 1px solid #e7e7e7;
+    background-color: #f3f3f3;
+}
+
+#topnav li {
+    float:left;
+     border-right:1px solid #bbb;
+}
+
+#topnav li:last-child {
+    border-right: none;
+}
+
+
+#topnav li a {
+    display: block;
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+   }
+#topnav li a:hover:not(.active) {
+    background-color: lightblue;
+}
+
+
+
+.active {
+    background-color:lightgreen;
+}
+
+#nav{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+h1 {
+  color: #f79209;
+   font-family: Arial, Helvetica, sans-serif;
+  font-size: 70px;
+  font-weight: 400;
+  line-height: 60px;
+  margin: 0;
+}
+
+
+h2 {
+  color:black;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 20px;
+  font-weight: 10;
+  line-height: 25px;
+  margin: 0;
+ 
+}
+
+#aboutus{
+     color: black;
+  font-family: 'Quicksand', sans-serif;
+  font-size: 50px;
+  font-weight: 400;
+  line-height: 60px;
+  margin: 0;
+}
+
+#us{
+    color: black;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 18px;
+  font-weight: 10;
+  line-height: 25px;
+  margin: 0;
+
+}
+
+        </style>
+        
+    </head>
+
+    <body>
+        <div id="pagewrap"> 
+        <!-- naviation bar will come here -->
+
+        <div id="topnav">
+            <ul class="nav">
+                <li><a href="/index.html">Home</a></li>
+                <li><a href="/services.html">Services</a></li>
+                <li><a href="/solution.html">Solutions</a></li>
+                <li><a class="active" href="/turnkey.html">Turnkey</a></li>
+                <li><a href="/about.html">About Us</a></li>
+                <li style="float:right"><a href="/contact.html">Contact</a></li>
+                
+
+            </ul>
+        </div>
+
+        <div id="padd">
+            </div>
+
+
+
+        <div>
+            <h2 align= 'center'>Turn Key Projects</h2>
+                
+                
+                <img src="http://www.pilotservicesinc.com/img/slider/slide2.jpg" alt="" width="505" height="300">
+                <img src="http://www.mayfront.com/wp-content/uploads/iStock_000005825614XSmall.jpg" alt="" width="350" height="300">
+                
+                
+                
+                
+                
+                <p id="us"> 
+                    
+                    We execute turnkey projects involved in Networking, Wireless Networking, VPN, Security, Storage, Servers, Desktops and Laptops sourcing which completes total execution of the project.</br>
+                    We are always striving to exceed your expectation for quality.</br>
+                    As part of SSTechnologies services portfolio Consultancy, Implementation and Managed services, our approach insures quality because we pay so much attention to every detail of a project including Designing of the Network, Selection of the hardware (Security, Storage, Servers, Software, Desktops & Laptops), managing suppliers, Complete implementation and keeping clients informed throughout the entire process. 
+                    
+                </p>
+        </div>
+        </div>
+    </body>
+</html>
